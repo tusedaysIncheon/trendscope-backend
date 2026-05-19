985c3f7713add4e8aa597305304fad77da603c53
Jeonjaeman <wjswoaks741@gmail.com>
2026-05-19 18:43:43 +0900

    server change

--- src/main/java/com/trendscope/backend/global/config/S3Config.java
+++ src/main/java/com/trendscope/backend/global/config/S3Config.java
@@ -3,7 +3,9 @@ package com.trendscope.backend.global.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -13,7 +15,7 @@ import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 public class S3Config {
 
     @Value("${cloud.aws.credentials.access-key}")
-    private String acccessKey;
+    private String accessKey;
     @Value("${cloud.aws.credentials.secret-key}")
     private String secretKey;
     @Value("${cloud.aws.region.static}")
@@ -24,9 +26,7 @@ public class S3Config {
     public S3Presigner s3Presigner() {
         return S3Presigner.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(acccessKey, secretKey)
-                ))
+                .credentialsProvider(credentialsProvider())
                 .build();
     }
 
@@ -34,10 +34,18 @@ public class S3Config {
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(acccessKey, secretKey)
-                ))
+                .credentialsProvider(credentialsProvider())
                 .build();
     }
 
+    private AwsCredentialsProvider credentialsProvider() {
+        if (hasText(accessKey) && hasText(secretKey)) {
+            return StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey));
+        }
+        return DefaultCredentialsProvider.create();
+    }
+
+    private boolean hasText(String value) {
+        return value != null && !value.isBlank();
+    }
 }
